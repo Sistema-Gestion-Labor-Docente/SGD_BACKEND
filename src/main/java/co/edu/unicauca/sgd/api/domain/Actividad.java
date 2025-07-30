731176a5fac80a27f070f5227b00b807533a54fc
Edwin Adrada Ruiz <jesusadrada@unicauca.edu.co>
2025-07-30 15:41:24 -0500

    Limpieza de imports

--- src/main/java/co/edu/unicauca/sgd/api/domain/Actividad.java
+++ src/main/java/co/edu/unicauca/sgd/api/domain/Actividad.java
@@ -6,8 +6,6 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
-import java.util.List;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Data
 @Entity
