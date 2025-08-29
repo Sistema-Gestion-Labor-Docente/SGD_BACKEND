15f80ad96704fa00abce60267a83e12e0584224c
Edwin Adrada Ruiz <jesusadrada@unicauca.edu.co>
2025-08-28 21:25:31 -0500

    Se actualiza temporalmente la seguridad.

--- src/main/java/co/edu/unicauca/sgd/api/config/SecurityConfig.java
+++ src/main/java/co/edu/unicauca/sgd/api/config/SecurityConfig.java
@@ -37,7 +37,8 @@ public class SecurityConfig {
                 .requestMatchers(
                     "/api/auth/**",
                     "/swagger-ui/**",
-                    "/v3/api-docs/**"
+                    "/v3/api-docs/**",
+                    "/api/**" //TODO: Remove this line in production
                 ).permitAll()
                 .anyRequest().authenticated()
             )
