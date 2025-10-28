2c4241e18eba6762b51e400849229e51ca906ba9
Edwin Adrada Ruiz <jesusadrada@unicauca.edu.co>
2025-10-27 22:53:01 -0500

    Adicion de cambios de estados para necesidades

--- src/main/java/co/edu/unicauca/sgd/api/service/necesidad/NecesidadService.java
+++ src/main/java/co/edu/unicauca/sgd/api/service/necesidad/NecesidadService.java
@@ -1,12 +1,12 @@
 package co.edu.unicauca.sgd.api.service.necesidad;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
 import co.edu.unicauca.sgd.api.dto.ApiResponse;
 import co.edu.unicauca.sgd.api.dto.necesidades.NecesidadDTORequest;
 import co.edu.unicauca.sgd.api.dto.necesidades.NecesidadDTOResponse;
 import co.edu.unicauca.sgd.api.enums.EstadoNecesidad;
+import java.util.Map;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface NecesidadService {
 
@@ -23,4 +23,10 @@ public interface NecesidadService {
     ApiResponse<NecesidadDTOResponse> actualizar(Integer oid, NecesidadDTORequest request);
 
     ApiResponse<Void> eliminar(Integer oid);
+
+    ApiResponse<Map<String, Object>> cambiarEstadoMasivo(Integer oidCalendario,
+                                                         EstadoNecesidad estadoOrigen,
+                                                         EstadoNecesidad estadoDestino,
+                                                         Integer oidPrograma,
+                                                         Integer oidDepartamento);
 }
